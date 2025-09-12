0cd037098a50dbf2d540c2e82e4871deee114223
Tu-lho <167235216+Tu-lho@users.noreply.github.com>
2025-09-12 19:17:28 -0300

    Delete pages directory

--- pages/pagina_de_abertura.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-def pagina_de_abertura():
-    st.title("🎓Minerva")    
-    st.write("Bem vindo ao app de organização academica!")
-    if st.button("Entrar"):
-        st.switch_page("pagina_de_login.py")
-    
-    st.button("Cadastrar")
