980fd9b3102c4fa72a5487bc163c7ee6da9c547f
LuisC <luis.coelho.761@gmail.com>
2025-10-28 21:42:15 -0300

    Adiciona endpoint de Notas para o client

---
+++ api_client/GradeEndpoint.py
@@ -0,0 +1,38 @@
+from api_client.client import ApiClient
+
+class GradeEndpoint:
+
+    def __init__(self, client: ApiClient):
+        self.client = client
+
+    def list_grades_by_discipline(self, discId: int):
+        """GET /notas/me/{discId} - lista todas as notas de uma disciplina"""
+        return self.client.get(f"notas/me/{discId}")
+    
+    def create_grade(self, descricao: str, valor: float, peso: int, disciplinaId: int):
+        """POST /notas/me/create - cria uma nova nota para o usuário logado"""
+        return self.client.post(
+            "notas/me/create", 
+            data={
+            "descricao": descricao, 
+            "valor": valor, 
+            "peso": peso, 
+            "disciplinaId": disciplinaId, 
+            }
+        )
+    
+    def update_grade(self, gradeId: int, descricao: str, valor: float, peso: int, disciplinaId: int):
+        """PUT /notas/me/{discId} - atualiza uma nota pelo ID"""
+        return self.client.put(
+            f"notas/me/{gradeId}", 
+            data={
+            "descricao": descricao, 
+            "valor": valor, 
+            "peso": peso, 
+            "disciplinaId": disciplinaId, 
+            }
+        )
+    
+    def delete_grade(self, gradeId: int):
+        """DELETE /notas/me/{gradeId} - apaga uma nota pelo ID"""
+        return self.client.delete(f"notas/me/{gradeId}")
