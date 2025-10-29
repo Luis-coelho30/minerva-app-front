b4a58f2103bc15b3e8ae1cd57705a9a1667dcd17
LuisC <luis.coelho.761@gmail.com>
2025-10-28 22:14:24 -0300

    Adiciona endpoint de Arquivos ao client

---
+++ api_client/endpoints/FileEndpoint.py
@@ -0,0 +1,42 @@
+from api_client.client import ApiClient
+
+class FileEndpoint:
+
+    def __init__(self, client: ApiClient):
+        self.client = client
+
+    def list_files(self):
+        """GET /arquivos/me - lista todas os arquivos do usuario logado"""
+        return self.client.get(f"arquivos/me")
+    
+    def list_files_by_discipline(self, discId: int):
+        """GET /arquivos/me/{discId} - lista todas os arquivos de uma disciplina pelo ID"""
+        return self.client.get(f"arquivos/me/{discId}")
+    
+    def create_file(self, disciplinaId: int, nomeOriginal: str, url: str, tipo: str):
+        """POST /arquivos/me/create - cria um novo arquivo para o usuário logado"""
+        return self.client.post(
+            "arquivos/me/create", 
+            data={
+            "disciplinaId": disciplinaId, 
+            "nomeOriginal": nomeOriginal, 
+            "url": url, 
+            "tipo": tipo, 
+            }
+        )
+    
+    def update_file(self, fileId: int, disciplinaId: int, nomeOriginal: str, url: str, tipo: str):
+        """PUT /arquivos/me/{fileId} - atualiza um arquivo pelo ID"""
+        return self.client.put(
+            f"arquivos/me/{fileId}", 
+            data={
+            "disciplinaId": disciplinaId, 
+            "nomeOriginal": nomeOriginal, 
+            "url": url, 
+            "tipo": tipo, 
+            }
+        )
+    
+    def delete_file(self, fileId: int):
+        """DELETE /arquivos/me/{fileId} - apaga uma nota pelo ID"""
+        return self.client.delete(f"arquivos/me/{fileId}")
